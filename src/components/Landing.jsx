e9ba7cd850875fb61af1bfc803043c83b0774c13
Ayan Manna <ayanm20183@gmail.com>
2026-04-30 17:31:46 +0530

    imports changed.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,20 +2,15 @@ import Banner from "./Banner";
 import PremiumSelection from "./PremiumSelection";
 import NatureElement from "./NatureElement";
 import WithUs from "./WithUs";
-import Footer from "./Footer";
 import Awards from "./Awards";
 import Blog from "./Blog";
 import Testimonials from "./Testimonials";
 import Subscribe from "./Subscribe";
 import FAQ from "./FAQ";
-import Nav from "./Nav";
-import Offers from "./Offers";
 
 const Landing = () => {
   return (
     <>
-      <Offers/>
-      <Nav />
       <Banner />
       <NatureElement />
       <PremiumSelection />
@@ -25,7 +20,6 @@ const Landing = () => {
       <Testimonials />
       <Subscribe />
       <FAQ />
-      <Footer />
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Landing from "../components/Landing";
+import Offers from "../components/Offers";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <Offers />
+      <Nav />
       <Landing />
+      <Footer />
     </>
   );
 };
